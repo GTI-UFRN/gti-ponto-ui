a68edcb5ad124c9fcc8f843e80cd7cb7520135ac
Arthur Ribeiro <devarthurribeiro@gmail.com>
2018-09-19 11:47:09 -0300

    Inclusão da rota de espelho do ponto

---
+++ src/pages/History.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>espelho</h1>
+</template>
+
+<script>
+  export default {
+    name: "HistoryView",
+    data() {
+      return {}
+    },
+    methods: {},
+    created() {}
+  }
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,8 @@ const routes = [
     path: '/',
     component: () => import('layouts/MyLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: 'history', component: () => import('pages/History.vue') }
     ]
   },
   {
