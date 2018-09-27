d597b055a34928b47180e9f8daafb38b7cc849df
Arthur Ribeiro <devarthurribeiro@gmail.com>
2018-09-27 11:40:50 -0300

    alterar url api dinamicamente

--- src/services/ponto/instace.js
+++ src/services/ponto/instace.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'http://localhost:3000/ponto'
+const baseURL = (process.env.NODE_ENV === 'development') ? 'http://localhost:3000/ponto' : 'http://10.77.0.11:3000/ponto'
 
 export default axios.create({
   baseURL,
