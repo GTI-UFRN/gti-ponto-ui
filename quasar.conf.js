40deb9320ad333b5e21d6b80fed52cc4b8c86d27
Arthur Ribeiro <devarthurribeiro@gmail.com>
2018-10-26 11:04:39 -0300

    Configurado suporte a pwa

--- quasar.conf.js
+++ quasar.conf.js
@@ -77,40 +77,21 @@ module.exports = function (ctx) {
       pwa: false
     },
     pwa: {
-      // workboxPluginMode: 'InjectManifest',
-      // workboxOptions: {},
+      workboxPluginMode: 'InjectManifest',
+      workboxOptions: {},
       manifest: {
-        // name: 'Quasar App',
-        // short_name: 'Quasar-PWA',
-        // description: 'Best PWA App in town!',
+        name: 'Ponto GTI',
+        short_name: 'Ponto GTI',
+        description: 'Sistema de Ponto Gerência de Tecnologia da Informação - EAJ',
         display: 'standalone',
         orientation: 'portrait',
-        background_color: '#ffffff',
-        theme_color: '#027be3',
+        background_color: '#fff',
+        theme_color: '#000',
+        start_url: "/",
         icons: [
           {
-            'src': 'statics/icons/icon-128x128.png',
-            'sizes': '128x128',
-            'type': 'image/png'
-          },
-          {
-            'src': 'statics/icons/icon-192x192.png',
+            'src': 'statics/icons/favicon.png',
             'sizes': '192x192',
-            'type': 'image/png'
-          },
-          {
-            'src': 'statics/icons/icon-256x256.png',
-            'sizes': '256x256',
-            'type': 'image/png'
-          },
-          {
-            'src': 'statics/icons/icon-384x384.png',
-            'sizes': '384x384',
-            'type': 'image/png'
-          },
-          {
-            'src': 'statics/icons/icon-512x512.png',
-            'sizes': '512x512',
             'type': 'image/png'
           }
         ]
