3e3f6d8618fe090e8487eb9e30ef9855ac42f04f
Arthur Ribeiro <devarthurribeiro@gmail.com>
2018-11-19 10:29:48 -0300

    Formulario de usuario agora é um component

---
+++ src/components/UserForm.vue
@@ -0,0 +1,135 @@
+<template>
+  <div style="padding: 16px">
+    <h1 class="q-headline">Dados básicos</h1>
+    <form @submit.prevent="save">
+      <div class="row gutter-sm">
+        <div class="col-xs-12 col-md-3">
+          <q-field>
+            <q-input v-model="user.name" float-label="Nome"  />
+          </q-field>
+        </div>
+        <div class="col-xs-12 col-md-3">
+          <q-field>
+            <q-input v-model="user.email" float-label="Email"  />
+          </q-field>
+        </div>
+        <div class="col-xs-12 col-md-3">
+          <q-field>
+            <q-input v-model="user.registration" float-label="Matrícula"  />
+          </q-field>
+        </div>
+        <div class="col-xs-12 col-md-3">
+          <q-field>
+            <q-datetime v-model="user.birthday" format="DD/MM/YY" type="date" float-label="Nascimento"/>
+          </q-field>
+        </div>
+      </div>
+      <h1 class="q-headline">Dados bancários</h1>
+      <div class="row gutter-sm">
+        <div class="col-xs-12 col-md-3">
+          <q-field>
+            <q-input v-model="user.bankAccount.agency" float-label="Agência"  />
+          </q-field>
+        </div>
+        <div class="col-xs-12 col-md-3">
+          <q-field>
+            <q-input v-model="user.bankAccount.account" float-label="Conta"  />
+          </q-field>
+        </div>
+        <div class="col-xs-12 col-md-3">
+          <q-field>
+            <q-select
+              v-model="user.bankAccount.bank"
+              float-label="Status"
+              radio
+              :options="banks"
+              />
+          </q-field>
+        </div>
+        <div class="col-xs-12 col-md-3">
+          <q-field>
+            <q-input v-model="user.bankAccount.operation" float-label="Operação"/>
+          </q-field>
+        </div>
+      </div>
+      <h1 class="q-headline">Dados da bolsa</h1>
+      <div class="row gutter-sm">
+        <div class="col-xs-12 col-md-3">
+          <q-field>
+            <q-datetime v-model="user.createdAt" float-label="Data Entrada"  />
+          </q-field>
+        </div>
+        <div class="col-xs-12 col-md-3">
+          <q-field>
+            <q-datetime v-model="user.exitAt" float-label="Data saida"  />
+          </q-field>
+        </div>
+        <div class="col-xs-12 col-md-3">
+          <q-field>
+            <q-select
+              v-model="user.active"
+              float-label="Status" radio :options="[{value: true, label: 'Ativo'}, {value: false, label: 'Inativo'}]"
+              />
+          </q-field>
+        </div>
+        <div class="col-xs-12 col-md-12">
+          <q-field>
+            <q-input v-model="user.operation" float-label="Observação"/>
+          </q-field>
+        </div>
+      </div>
+      <h1 class="q-headline">Dados de endereço</h1>
+      <div class="row gutter-sm">
+        <div class="col-xs-12 col-md-3">
+          <q-field>
+            <q-input v-model="user.address.cep" float-label="Cep"  />
+          </q-field>
+        </div>
+        <div class="col-xs-12 col-md-3">
+          <q-field>
+            <q-input v-model="user.address.number" float-label="Número"  />
+          </q-field>
+        </div>
+        <div class="col-12">
+          <p>
+            {{address.localidade}} - {{address.uf}} <br/>
+            {{address.logradouro || 'Centro'}} <span v-if="address.bairro"> - {{address.bairro}}</span> <br/>
+          </p>
+        </div>
+      </div>
+      <br/>
+      <q-btn type="submit" color="green">
+        Salvar
+      </q-btn>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserForm',
+  props: ['user'],
+  data () {
+    return {
+      address: {},
+      banks: [
+        { value: '001', label: '001 - BANCO DO BRASIL S/A' },
+        { value: '104', label: '104 - CAIXA ECONOMICA FEDERAL' }
+      ]
+    }
+  },
+  methods: {
+    getCep (cep) {
+      const url = `https://viacep.com.br/ws/${cep}/json`
+      fetch(url)
+        .then(r => r.json())
+        .then(r => {
+          this.address = r
+        })
+    },
+    save () {
+      this.$emit('onSave', this.user)
+    }
+  }
+}
+</script>
--- src/pages/UsersManager.vue
+++ src/pages/UsersManager.vue
@@ -13,123 +13,17 @@
     @request="request"
   >
     <div slot="top-right" slot-scope="props" class="column">
-      <q-btn @click="user = { bankAccount: {}, address: {} }; showUserForm()" icon="note_add" color="green">
+      <q-btn @click="addUser" icon="note_add" color="green">
         Novo usuário
       </q-btn>
     </div>
     <template slot="top-selection" slot-scope="props">
-      <q-btn @click="user = selected[0]; showUserForm()" color="primary" flat label="Editar" class="q-mr-sm" />
+      <q-btn @click="editUser" color="primary" flat label="Editar" class="q-mr-sm" />
       <div class="col" />
     </template>
   </q-table>
-   <q-modal v-model="showForm" maximized>
-    <div style="padding: 16px">
-      <h1 class="q-headline">Dados básicos</h1>
-      <form>
-        <div class="row gutter-sm">
-          <div class="col-xs-12 col-md-3">
-            <q-field>
-              <q-input v-model="user.name" float-label="Nome"  />
-            </q-field>
-          </div>
-          <div class="col-xs-12 col-md-3">
-            <q-field>
-              <q-input v-model="user.email" float-label="Email"  />
-            </q-field>
-          </div>
-          <div class="col-xs-12 col-md-3">
-            <q-field>
-              <q-input v-model="user.registration" float-label="Matrícula"  />
-            </q-field>
-          </div>
-          <div class="col-xs-12 col-md-3">
-            <q-field>
-              <q-datetime v-model="user.birthday" format="DD/MM/YY" type="date" float-label="Nascimento"/>
-            </q-field>
-          </div>
-        </div>
-        <h1 class="q-headline">Dados bancários</h1>
-        <div class="row gutter-sm">
-           <div class="col-xs-12 col-md-3">
-            <q-field>
-              <q-input v-model="user.bankAccount.agency" float-label="Agência"  />
-            </q-field>
-          </div>
-          <div class="col-xs-12 col-md-3">
-            <q-field>
-              <q-input v-model="user.bankAccount.account" float-label="Conta"  />
-            </q-field>
-          </div>
-          <div class="col-xs-12 col-md-3">
-            <q-field>
-              <q-select
-                  v-model="user.bankAccount.bank"
-                  float-label="Status"
-                  radio
-                :options="banks"
-                />
-            </q-field>
-          </div>
-          <div class="col-xs-12 col-md-3">
-            <q-field>
-              <q-input v-model="user.bankAccount.operation" float-label="Operação"/>
-            </q-field>
-          </div>
-        </div>
-
-        <h1 class="q-headline">Dados da bolsa</h1>
-        <div class="row gutter-sm">
-           <div class="col-xs-12 col-md-3">
-            <q-field>
-              <q-datetime v-model="user.createdAt" float-label="Data Entrada"  />
-            </q-field>
-          </div>
-          <div class="col-xs-12 col-md-3">
-            <q-field>
-              <q-datetime v-model="user.exitAt" float-label="Data saida"  />
-            </q-field>
-          </div>
-          <div class="col-xs-12 col-md-3">
-            <q-field>
-               <q-select
-                  v-model="user.active"
-                  float-label="Status"
-                  radio
-                :options="[{value: true, label: 'Ativo'}, {value: false, label: 'Inativo'}]"
-                />
-            </q-field>
-          </div>
-          <div class="col-xs-12 col-md-12">
-            <q-field>
-              <q-input v-model="user.operation" float-label="Observação"/>
-            </q-field>
-          </div>
-        </div>
-        <h1 class="q-headline">Dados de endereço</h1>
-        <div class="row gutter-sm">
-           <div class="col-xs-12 col-md-3">
-            <q-field>
-              <q-input v-model="user.address.cep" float-label="Cep"  />
-            </q-field>
-          </div>
-          <div class="col-xs-12 col-md-3">
-            <q-field>
-              <q-input v-model="user.address.number" float-label="Número"  />
-            </q-field>
-          </div>
-          <div class="col-12">
-            <p>
-              {{address.localidade}} - {{address.uf}} <br/>
-              {{address.logradouro || 'Centro'}} <span v-if="address.bairro"> - {{address.bairro}}</span> <br/>
-            </p>
-          </div>
-        </div>
-        <br>
-        <q-btn @click="showForm = false" color="green">
-          Salvar
-        </q-btn>
-      </form>
-    </div>
+  <q-modal v-model="showForm" maximized>
+    <user-form @onSave="save" :user="user" />
   </q-modal>
   </q-page>
 </template>
@@ -137,6 +31,7 @@
 <script>
 import moment from 'moment'
 import users from '../services/users'
+import UserForm from '../components/UserForm'
 
 const tableColumns = [
   {
@@ -183,22 +78,31 @@ export default {
   name: 'UsersManager',
   data () {
     return {
-      banks: [
-        { value: '001', label: '001 - BANCO DO BRASIL S/A' },
-        { value: '104', label: '104 - CAIXA ECONOMICA FEDERAL' }
-      ],
       columns: tableColumns,
       selected: [],
       users: [],
       address: {},
       user: {
-        bankAccount: {},
-        address: {}
+        address: {},
+        bankAccount: {}
       },
       showForm: false
     }
   },
+  components: {
+    UserForm
+  },
   methods: {
+    addUser () {
+      this.user = { address: {}, bankAccount: {} }
+      this.showUserForm()
+    },
+    editUser () {
+      this.user = this.selected[0]
+      if (!this.user.bankAccount) this.user.bankAccount = {}
+      if (!this.user.address) this.user.address = {}
+      this.showUserForm()
+    },
     getCep (cep) {
       const url = `https://viacep.com.br/ws/${cep}/json`
       fetch(url)
@@ -207,11 +111,14 @@ export default {
           this.address = r
         })
     },
+    save (user) {
+      this.showForm = false
+    },
     showUserForm () {
       this.showForm = true
-      if (this.user.address.cep && this.user.address.cep !== '') {
-        this.getCep(this.user.address.cep)
-      }
+      // if (this.user.address.cep && this.user.address.cep !== '') {
+      //   this.getCep(this.user.address.cep)
+      // }
     },
     async request () {
       try {
